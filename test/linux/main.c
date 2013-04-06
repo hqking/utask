5c1ffec38c5f7ca399ed0ac3dd60ae6cf5f6e22a
Yifeng Jin <hqking@gmail.com>
2013-04-06 22:32:25 +0800

    temp commit, debug version

--- test/linux/main.c
+++ test/linux/main.c
@@ -27,13 +27,19 @@ void taskIdleHook(time_t delay)
 {
   time_t until;
 
+  printf("idle hook %ds\n", delay);
+
+  exit(1); 
+
   until = time(NULL) + delay;
 
   g_wake = 0;
   while(until > time(NULL)) {
-    if (g_wake)
+    if (g_wake) {
+      printf("wakened\n");
       break;
-  }    
+    }
+  }
 }
 
 void taskWake(void)
@@ -48,17 +54,35 @@ static FILE *g_log;
 static void isr_ctx(int sig)
 {
   /* read from pipe to know the actions to be taken */
+  g_wake = 1;
 }
 
 static void loop_ctx(void)
 {
+  signal(SIGINT, isr_ctx);
+
   taskInit();
   taskSchedule();
+  /* while (1) { */
+  /*   if (g_wake == 1) { */
+  /*     g_wake = 0; */
+  /*     printf("oh, good\n"); */
+  /*   } */
+  /* } */
 }
 
 static void outsidEvents(pid_t pid)
 {
+  int rc;
+
   /* fire event according to test case */
+  while (1) {
+    sleep(1);
+    printf("fire\n");
+    rc = kill(pid, SIGINT);
+    if (rc == -1)
+      printf("error\n");
+  }
 }
 
 static void logCreate(void)
@@ -78,8 +102,6 @@ int main(int argc, char *argv[])
 {
   pid_t pid;
 
-  signal(SIGINT, isr_ctx);
-
   /* open log files, in cvs format */
   logCreate();
 
@@ -88,10 +110,11 @@ int main(int argc, char *argv[])
   if (pid == -1)
     return -1;
 
-  if (pid == 0)
+  if (pid == 0) {
     loop_ctx();
-  else
+  } else {
     outsidEvents(pid);
+  }
 
   fclose(g_log);
 
